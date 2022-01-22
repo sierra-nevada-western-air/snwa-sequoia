dc690492089bd50c5ce073accd3e03fcc7fbe37c
Michael Bradvica <mjbradvica@protonmail.com>
2022-01-22 14:54:31 -0500

    removed default storybook

--- src/features/layout/section/Section.stories.tsx
+++ src/features/layout/section/Section.stories.tsx
@@ -1,12 +1,16 @@
 import Section from './Section';
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta } from '@storybook/react';
+import { SectionSize } from './SectionSize';
 
 const SectionStory: ComponentMeta<typeof Section> = {
-	title: 'Section',
+	title: 'Layout/Section',
 	component: Section,
 };
 
 export default SectionStory;
 
 export const Small = (): JSX.Element => <Section>I am a section.</Section>;
+
+export const Medium = (): JSX.Element => <Section size={SectionSize.Medium}>I am a medium section.</Section>;
+
+export const Large = (): JSX.Element => <Section size={SectionSize.Large}>I am a large section.</Section>;
