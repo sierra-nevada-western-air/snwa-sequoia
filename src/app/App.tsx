6e4688cbbf5fb792f156811b95fb05f59a9783a1
Michael Bradvica <mjbradvica@protonmail.com>
2022-01-22 13:53:55 -0500

    updated pre commmit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-	return <div>Hello!</div>;
+	return <div className="text-teal-500 font-semibold">Hello!</div>;
 };
 
 export default App;
